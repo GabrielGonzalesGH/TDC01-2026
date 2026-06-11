a55843ec20c62bd2a4128d82d541afc233e45cb0
GabrielGonzalesGH <GabrielGonzalesGH@users.noreply.github.com>
2026-06-11 04:10:15 -0300

    Create config.h

---
+++ config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+extern char openSET;
+extern char closeSET;
+extern char openLIST;
+extern char closeLIST;
+extern char separador;
+
+void configUsuario(void);
+void limpiar_buffer(void);
+
+#endif
